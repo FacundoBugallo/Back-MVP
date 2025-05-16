0b27847387648c038b116444f3eb3e0b7195a932
Facundo Bugallo <facundobugallo881@gmail.com>
2025-05-16 00:26:31 -0300

    feat 0.4

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
+from app.routers import user, income
 from app.database import Base, engine
-from app.routers import user 
 
 app = FastAPI()
 Base.metadata.create_all(bind=engine)
 
 app.include_router(user.router)
-
+app.include_router(income.router)  
 @app.get("/")
 def read_root():
     return {"message": "API funcionando"}
