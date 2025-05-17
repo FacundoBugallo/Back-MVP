31f4b7c898aca8de91271abff5df4f9930d87f06
Facundo Bugallo <facundobugallo881@gmail.com>
2025-05-17 20:08:36 -0300

    CRUD CATEGORIAS

--- app/schemas/category.py
+++ app/schemas/category.py
@@ -1,18 +1,25 @@
 from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional, Literal
 
-
+# Base
 class CategoryBase(BaseModel):
     name: str
-    type: str  
+    type: Literal["income", "expense"]
 
-
+# Crear
 class CategoryCreate(CategoryBase):
     pass
 
+# Actualizar
+class CategoryUpdate(BaseModel):
+    name: Optional[str] = None
+    type: Optional[Literal["income", "expense"]] = None
 
+# Leer
 class CategoryOut(CategoryBase):
     id: int
+    user_id: int
     created_at: datetime
 
     class Config:
