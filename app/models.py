8705a2b9b0c27844f64ee45159d8927dd43711e5
Facundo Bugallo <facundobugallo881@gmail.com>
2025-05-17 20:08:07 -0300

    fix

--- app/models.py
+++ app/models.py
@@ -50,15 +50,15 @@ class Expense(Base):
     user = relationship("User", back_populates="expenses")
     category = relationship("Category")
 
-
 class Income(Base):
     __tablename__ = "incomes"
+
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, ForeignKey("users.id"))
+    user_id = Column(Integer, ForeignKey("users.id"), nullable=False) 
     amount = Column(Numeric(12, 2), nullable=False)
     description = Column(Text)
     date = Column(Date, nullable=False)
     category_id = Column(Integer, ForeignKey("categories.id"))
 
     user = relationship("User", back_populates="incomes")
-    category = relationship("Category")
+    category = relationship("Category") 
